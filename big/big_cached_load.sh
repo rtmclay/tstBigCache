c05236e4e8e6372362dfccfe61ab522e5258fd70
Robert McLay <mclay@zaniahgroup.com>
2025-06-08 21:20:37 -0500

    cached and non-cached tests add

---
+++ big/big_cached_load.sh
@@ -0,0 +1,2 @@
+LMOD_CACHED_LOADS=1 MODULEPATH=$PWD/modulefiles LMOD_RC=$PWD/lmodrc.lua $LMOD_CMD shell --timer load BIG > stuff/stdout
+
--- small/small_test.sh
+++ small/small_test.sh
@@ -1,1 +1,2 @@
-LMOD_CACHED_LOADS=1 MODULEPATH=$PWD/modulefiles LMOD_RC=$PWD/lmodrc.lua $LMOD_CMD shell --timer load small > ~/t/Lmod_Testing/bigCache/small_stdout 
+MODULEPATH=$PWD/modulefiles LMOD_RC=$PWD/lmodrc.lua $LMOD_CMD shell --timer load small > $PWD/stuff/stdout
+
