0487b88a98050a9fbf1eb585aba24bf18385b261
Robert McLay <mclay@zaniahgroup.com>
2025-06-08 21:08:41 -0500

    updating big test

--- big/big_setup.sh
+++ big/big_setup.sh
@@ -8,8 +8,9 @@ if [ $SCRIPT_DIR = "." ]; then
   SCRIPT_DIR=$PWD
 fi  
 
-CacheDir=$SCRIPT_DIR/cacheDir
-RmapDir=$SCRIPT_DIR/rmapDir
+CacheDir=$SCRIPT_DIR/stuff/cacheDir
+RmapDir=$SCRIPT_DIR/stuff/rmapDir
+SysFn=$SCRIPT_DIR/stuff/system.txt
 mkdir -p $CacheDir $RmapDir
-$LMOD_DIR/spiderCacheSupport --cacheDirs $CacheDir --updateFn $SCRIPT_DIR/system.txt > $SCRIPT_DIR/lmodrc.lua
-$LMOD_DIR/update_lmod_system_cache_files -t $SCRIPT_DIR/system.txt -d $CacheDir -R $RmapDir -J -K $SCRIPT_DIR/modulefiles
+$LMOD_DIR/spiderCacheSupport --cacheDirs $CacheDir --updateFn $SysFn > $SCRIPT_DIR/lmodrc.lua
+$LMOD_DIR/update_lmod_system_cache_files -t $SysFn -d $CacheDir -R $RmapDir -J -K $SCRIPT_DIR/modulefiles
