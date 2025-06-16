b23c63ef0c262fe1bfdedc208424e3614160bc7f
Robert McLay <mclay@zaniahgroup.com>
2025-06-16 16:07:55 -0500

    adding debugging tests

--- small/dbg_small_test.sh
+++ small/dbg_small_test.sh
@@ -1,1 +1,1 @@
-( module purge; clearMT; MODULEPATH=$PWD/modulefiles LMOD_RC=$PWD/lmodrc.lua $LMOD_CMD shell -D load small > stuff/stdout.normal stuff/stderr.normal )
+( module purge; clearMT; MODULEPATH=$PWD/modulefiles LMOD_RC=$PWD/lmodrc.lua $LMOD_CMD shell -D load small > stuff/stdout.normal 2> stuff/stderr.normal )
