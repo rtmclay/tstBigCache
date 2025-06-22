a9b26493e812e38b544500adb4bd7947acf49f9e
Robert McLay <mclay@zaniahgroup.com>
2025-06-22 18:19:04 -0500

    Adding tiny case

---
+++ tiny/tiny_load.sh
@@ -0,0 +1,1 @@
+( module purge; clearMT; MODULEPATH=$PWD/modulefiles LMOD_RC=$PWD/lmodrc.lua module load tiny ; ml --mt )
--- tiny/tiny_test.sh
+++ tiny/tiny_test.sh
@@ -1,1 +1,1 @@
-( module purge; clearMT; MODULEPATH=$PWD/modulefiles LMOD_RC=$PWD/lmodrc.lua $LMOD_CMD shell --timer load tiny > stuff/stdout.normal )
+( module purge; clearMT; MODULEPATH=$PWD/modulefiles LMOD_RC=$PWD/lmodrc.lua $LMOD_CMD shell --timer load tiny > stuff/stdout.normal; ml --mt )
