89467aae0d5626aa2bb8bb2d68c01774b6bda8a7
Robert McLay <mclay@zaniahgroup.com>
2025-06-27 14:28:20 -0500

    update big_test.sh to not set LMOD_CACHED_LOADS

--- big/big_test.sh
+++ big/big_test.sh
@@ -1,3 +1,3 @@
-( module purge; clearMT; MODULEPATH=$PWD/modulefiles LMOD_RC=$PWD/lmodrc.lua $LMOD_CMD shell --timer load BIG > /dev/null )
+( module purge; clearMT; unset LMOD_CACHED_LOADS; MODULEPATH=$PWD/modulefiles LMOD_RC=$PWD/lmodrc.lua $LMOD_CMD shell --timer load BIG > /dev/null )
 
 
